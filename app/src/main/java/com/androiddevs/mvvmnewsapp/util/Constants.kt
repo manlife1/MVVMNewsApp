df6bcf79f68774f5f03f39fb5add4dc1acdc3cff
Manish Raj <manishlife1@gmail.com>
2022-01-19 21:53:54 +0530

    Version 1

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY="1c1e15679fee4ef8845a7419f947ce61"
+        const val BASE_URL="https://newsapi.org"
+    }
+}
